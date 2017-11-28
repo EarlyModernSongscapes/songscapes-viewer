b4c28f01e845522dac0aaf682f77db01109208bc
Raff Viglianti <raffaeleviglianti@gmail.com>
2017-11-28 09:11:06 -0500

    adjusted path to test data

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use('/data', express.static('../data'))
+app.use('/data', express.static('./test/fakeData'))
 
 if (isDevelopment) {
   config.devtool = 'inline-source-map'
