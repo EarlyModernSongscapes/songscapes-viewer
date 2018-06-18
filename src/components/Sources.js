f1c9fe1a98a4531a0a855d6278dc23a3f945c687
raffazizzi <raffaeleviglianti@gmail.com>
2018-06-18 15:40:38 -0400

    using islandora urls

--- src/components/Sources.js
+++ src/components/Sources.js
@@ -9,11 +9,12 @@ export default class Sources extends Component {
     if (this.props.sources.length > 0) {
       sources = (<ul className="sourceList">
         {this.props.sources.map((s, i) => {
-          // TODO fix these string operations
-          const label = s.split('-')[1].split('.xml')[0]
-          const url = s.split('/')[3].split('.xml')[0].split('-')[1]
+          // TODO fix these string operations -- can we get these data from Islandora?
+          // const label = s.split('-')[1].split('.xml')[0]
+          // const url = s.split('/')[3].split('.xml')[0].split('-')[1]
+          const label = s.match(/datastream\/(.*?)\//)[1].split('-')[1]
           const active = s.includes(this.props.active) ? 'active' : 'inactive'
-          return <li key={i} className={active}><Link to={url}>{label}</Link></li>
+          return <li key={i} className={active}><Link to={label}>{label}</Link></li>
         })}
       </ul>)
     }
