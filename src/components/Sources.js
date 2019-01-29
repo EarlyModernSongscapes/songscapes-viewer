9109f69db8a7d8bd48ae4214817eb61ed66b7570
raffazizzi <raffaeleviglianti@gmail.com>
2019-01-29 15:39:28 -0500

    Added link to TEI and MEI source

--- src/components/Sources.js
+++ src/components/Sources.js
@@ -8,14 +8,28 @@ export default class Sources extends Component {
     let sources = 'loading'
     if (this.props.sources) {
       if (this.props.sources.tei) {
+        console.log(this.props.sources)
         sources = (<ul className="sourceList">
           {this.props.sources.tei.map((s, i) => {
             // TODO fix these string operations -- can we get these data from Islandora?
             // const label = s.split('-')[1].split('.xml')[0]
             // const url = s.split('/')[3].split('.xml')[0].split('-')[1]
-            const label = s.url.match(/datastream\/(.*?)\//)[1].split('-')[1]
+            // const label = s.url.match(/datastream\/(.*?)\//)[1].split('-')[1]
             const active = s.url.includes(this.props.active) ? 'active' : 'inactive'
-            return <li key={i} className={active}><Link to={label}>{label}</Link></li>
+            const meiSource = this.props.sources.mei.filter(ms => ms.source === s.source)[0]
+            const linkToMEI = meiSource ? meiSource.url : ''
+            const aToMEI = (<a href={linkToMEI} style={
+              {textDecoration: 'underline', visibility: meiSource ? 'visible' : 'hidden'}
+            }>MEI</a>)
+            return (
+              <li key={i} className={active}>
+                <Link to={s.source}>{s.source}</Link>
+                <span style={{float: 'right'}}>
+                  <a href={s.url} style={{textDecoration: 'underline'}}>TEI</a>&nbsp;&nbsp;
+                  {aToMEI}
+                </span>
+              </li>
+            )
           })}
         </ul>)
       }
